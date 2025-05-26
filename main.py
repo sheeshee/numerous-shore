443c128147046cccddff1a90b0c7927cd97a789b
Samuel Sheehy <samuelsheehy95@gmail.com>
2025-05-26 18:16:40 +0100

    webview shows a cancel button when in snooze mode

--- main.py
+++ main.py
@@ -70,6 +70,7 @@ clock_setting_timer.init(period=5000, mode=Timer.PERIODIC, callback=lambda _: di
 
 
 async def run_wake_up_sequence():
+    global alarm_mode
     # beep buzzer until button is pressed
     while not button.press.is_set():
         buzzer.duty_u16(DUTY)
@@ -77,6 +78,7 @@ async def run_wake_up_sequence():
         buzzer.duty_u16(0)
         await asyncio.sleep(0.5)
     button.press.clear()
+    alarm_mode = ALARM_MODE_SNOOZE  # Set alarm mode to snooze
 
     # itiate countdown
     for i in range(10, 0, -1):
@@ -86,6 +88,7 @@ async def run_wake_up_sequence():
         await asyncio.sleep(1)
 
     bell_pin.on()  # turn on bell
+    alarm_mode = ALARM_MODE_RINGING  # Set alarm mode to ringing
     await button.press.wait()  # wait for button press to stop ringing
     bell_pin.off()  # turn off bell
 
@@ -99,6 +102,7 @@ button.press_func = None
 buzzer = PWM(Pin(5), freq=200, duty_u16=0)
 
 bell_pin = Pin(0, Pin.OUT)
+bell_pin.off()  # ensure bell is off at start
 
 ALARM_MODE_OFF = 0
 ALARM_MODE_ON = 1
@@ -112,18 +116,43 @@ alarm_status_dict = {
     ALARM_MODE_RINGING: 'Ringing',
 }
 
-alarm_hour = 5
-alarm_minute = 0
-alarm_mode = ALARM_MODE_OFF
+alarm_hour = 17
+alarm_minute = 50
+alarm_mode = ALARM_MODE_OFF  # Start in snooze mode
 
 app = Microdot()
-with open('templates/index.html', 'r') as f:
-    template = f.read()
+
+with open('templates/set_alarm.html', 'r') as f:
+    set_alarm_template = f.read()
+
+with open('templates/cancel_countdown.html', 'r') as f:
+    cancel_countdown_template = f.read()
 
 @app.route('/', methods=['GET'])
 async def index(request):
     global alarm_hour, alarm_minute, alarm_mode
-    return template.replace('$alarm_time', f'{alarm_hour:02}:{alarm_minute:02}')\
+    print(alarm_status_dict[alarm_mode])
+    if alarm_mode == ALARM_MODE_SNOOZE:
+        bell_minute = (alarm_minute + 1)
+        bell_minute_adj = bell_minute % 60
+        bell_hour = (alarm_hour + bell_minute // 60) + 1
+        bell_hour_adj = bell_hour % 24
+
+        year, month, day, _, _, _, _, _ = RTC().datetime()
+
+        bell_day = day + (bell_hour // 24)
+        bell_day_adj = bell_day % 31  # Simplified, does not account for month length
+
+        bell_month = month + (bell_day // 31)
+        bell_month_adj = bell_month % 12
+        bell_year = year + (bell_month // 12)
+
+        bell_time = f'{bell_hour_adj:02}:{bell_minute_adj:02}'
+        date =  f'{bell_year:04}-{bell_month_adj:02}-{bell_day_adj:02}'
+        # if snoozed, show cancel countdown
+        return cancel_countdown_template.replace('$time', bell_time)\
+                .replace('$date', date)
+    return set_alarm_template.replace('$alarm_time', f'{alarm_hour:02}:{alarm_minute:02}')\
         .replace('$alarm_status', alarm_status_dict[alarm_mode])
 
 
@@ -138,7 +167,7 @@ async def set_alarm(request):
     alarm_mode = ALARM_MODE_ON
     # send new time to handle_alarm task
     broker.publish('alarm/set', (alarm_hour, alarm_minute))
-    return template.replace('$alarm_time', f'{alarm_hour:02}:{alarm_minute:02}')\
+    return set_alarm_template.replace('$alarm_time', f'{alarm_hour:02}:{alarm_minute:02}')\
         .replace('$alarm_status', alarm_status_dict[alarm_mode])
 
 @app.route('/toggle', methods=['POST'])
@@ -152,6 +181,16 @@ async def toggle_alarm(request):
         alarm_mode = ALARM_MODE_OFF
     # send new status to handle_alarm task
     broker.publish('alarm/status', alarm_mode)
+    return redirect('/')
+
+
+@app.route('/cancel', methods=['POST'])
+async def cancel_alarm(request):
+    global alarm_mode
+    if alarm_mode in (ALARM_MODE_RINGING, ALARM_MODE_SNOOZE):
+        alarm_mode = ALARM_MODE_ON
+        # send new status to handle_alarm task
+        broker.publish('alarm/status', alarm_mode)
     return redirect('/')
 
 
